fd15d5b75d64bb9aab90f9c4002f6d89592007dc
oJaciel <jacielhbarichellO@gmail.com>
2025-01-20 20:02:17 -0300

    Componente ProductGrid
    Extraída a grid de produtos da tela inicial, transformando o mesmo em um componente a parte.

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/components/product_grid.dart';
 import 'package:shop/components/product_item.dart';
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
@@ -10,25 +11,14 @@ class ProductsOverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-  final provider = Provider.of<ProductList>(context);
-
-  final List<Product> loadedProducts = provider.items;
+  
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3/2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (ctx, index) => ProductItem(loadedProducts[index]),
-      ),
+      body: ProductGrid(),
     );
   }
 }
+

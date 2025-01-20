38e63b6cea4c65d2efd85b127f9f7b7510bd7147
oJaciel <jacielhbarichellO@gmail.com>
2025-01-20 19:59:43 -0300

    Provider ProductList
    Criado um provider para a lista de produtos, que contém a lista de produtos e um método para adição de novo produto.
    
    Cadastrado o provider no main.
    
    Agora a lista de produtos obtida na tela inicial é oriunda da lista no provider.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -13,21 +15,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Shop App',
-      theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(
-          secondary: Colors.deepOrange,
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Shop App',
+        theme: ThemeData(
+          colorScheme:
+              ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(
+            secondary: Colors.deepOrange,
+          ),
+          fontFamily: 'Lato',
+          useMaterial3: true,
         ),
-        fontFamily: 'Lato',
-        useMaterial3: true,
+        home: ProductsOverviewPage(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
+        },
       ),
-      home: ProductsOverviewPage(),
-      routes: {
-        AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
-      },
     );
   }
 }

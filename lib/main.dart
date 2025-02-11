a7c269704e416d9ca3c90eb2ee180a9572cfb536
oJaciel <jacielhbarichellO@gmail.com>
2025-02-11 10:10:52 -0300

    Model e Provider para Order
    Criado um model de dados para os pedidos (Order).
    
    Criado o provider OrderList, para manipular os dados dos pedidos.
    
    Registrado o provider no main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/order_list.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
@@ -20,7 +21,8 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => ProductList(),),
-        ChangeNotifierProvider(create: (_) => Cart())
+        ChangeNotifierProvider(create: (_) => Cart()),
+        ChangeNotifierProvider(create: (_) => OrderList())
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,

eed4ad390a59d2281ac960b930642b9d7e2682cc
oJaciel <jacielhbarichellO@gmail.com>
2025-02-11 10:15:02 -0300

    Adicionando pedidos
    Implementado o método de adicionar pedidos na tela do carrinho. O método é disparado quando clicado no botão de 'Comprar', que agora também limpa os pedidos no carrinho.

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/cart_item.dart';
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/order_list.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -54,7 +55,14 @@ class CartPage extends StatelessWidget {
                       textStyle:
                           TextStyle(color: Theme.of(context).primaryColor),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<OrderList>(
+                        context,
+                        listen: false,
+                      ).addOrder(cart);
+
+                      cart.clear();
+                    },
                     child: Text('COMPRAR'),
                   ),
                 ],

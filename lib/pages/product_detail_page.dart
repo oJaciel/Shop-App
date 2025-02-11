347cae6e2270d6a19d5514326cd6babc737ee9a7
oJaciel <jacielhbarichellO@gmail.com>
2025-02-11 09:55:39 -0300

    Componente Detalhes de Produto
    Implementada a versão inicial para a tela dos detalhes de cada produto.

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -2,15 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
 
 class ProductDetailPage extends StatelessWidget {
-
   const ProductDetailPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-  final Product product = ModalRoute.of(context)!.settings.arguments as Product;
+    final Product product =
+        ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
         title: Text(product.name),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 300,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'R\$ ${product.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }

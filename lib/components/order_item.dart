6a1c1d5a1047cd35878fe3e101a38ccd19c20840
oJaciel <jacielhbarichellO@gmail.com>
2025-02-11 21:32:37 -0300

    Finalização Componente OrderItem
    Adicionado ao componente OrderItem a funcionalidade de mostrar os produtos que compõe o pedido.
    
    Agora, os itens de cada pedido são mostrados, com nome, quantidade e valor unitário de cada um.

--- lib/components/order_item.dart
+++ lib/components/order_item.dart
@@ -2,20 +2,70 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shop/models/order.dart';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final Order order;
 
   const OrderItem(this.order);
 
   @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool _expanded = false;
+
+  @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        title: Text('R\$ ${order.total.toStringAsFixed(2)}'),
-        subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(order.date)),
-        trailing: IconButton(onPressed: () {}, icon: Icon(Icons.expand_more)),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
+            subtitle:
+                Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)),
+            trailing: IconButton(
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+                icon: Icon(Icons.expand_more)),
+          ),
+          if (_expanded == true)
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              height: (widget.order.products.length * 25.0) + 10, //A quantidade de produts * 25, + 10 (pra garanti que dê um espaço no final)
+              child: ListView(
+                children: widget.order.products.map(
+                  (product) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          product.name,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '${product.quantity}x R\$ ${product.price}',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ).toList(),
+              ),
+            )
+        ],
       ),
-
     );
   }
 }

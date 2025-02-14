6ed8497511a84432610708e4d4e2b70157124d55
oJaciel <jacielhbarichellO@gmail.com>
2025-02-14 11:32:15 -0300

    Tela Formulário de Produto
    Definida a tela que será responsável pelo formulário dos produtos.
    
    Definidas as rotas e navegação para a tela, que é acessada a partir da tela de gerenciamento de produtos.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/orders_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
+import 'package:shop/pages/product_form_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/pages/products_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -43,6 +44,7 @@ class MyApp extends StatelessWidget {
           AppRoutes.CART: (ctx) => CartPage(),
           AppRoutes.ORDERS: (ctx) => OrdersPage(),
           AppRoutes.PRODUCTS: (ctx) => ProductsPage(),
+          AppRoutes.PRODUCT_FORM: (ctx) => ProductFormPage(),
         },
       ),
     );

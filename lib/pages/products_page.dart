2984081ceaa020ddc68b03dd21d3af768c36972f
oJaciel <jacielhbarichellO@gmail.com>
2025-02-14 11:31:02 -0300

    Componente Item Produto
    Criado o componente que é mostrado na tela de gerenciamento de produtos.
    
    O componente mostra os itens já cadastrados, com opções de edição e exclusão (a serem implementadas).

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/app_drawer.dart';
+import 'package:shop/components/product_item.dart';
 import 'package:shop/models/product_list.dart';
+import 'package:shop/utils/app_routes.dart';
 
 class ProductsPage extends StatelessWidget {
   const ProductsPage({super.key});
@@ -13,13 +15,25 @@ class ProductsPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Gerenciar Produtos'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(AppRoutes.PRODUCT_FORM);
+              },
+              icon: Icon(Icons.add))
+        ],
       ),
       drawer: AppDrawer(),
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
-          itemBuilder: (ctx, i) => Text(products.items[i].name),
           itemCount: products.itemsCount,
+          itemBuilder: (ctx, i) => Column(
+            children: [
+              ProductItem(products.items[i]),
+              Divider(),
+            ],
+          ),
         ),
       ),
     );

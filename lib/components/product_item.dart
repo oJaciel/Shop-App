30886266c8b55d5e23d24447ecee7dae095b9549
oJaciel <jacielhbarichellO@gmail.com>
2025-02-17 19:57:06 -0300

    Confirmar Exclusão de Produto
    Adicionado AlertDialog para confirmar a exclusão de um produto, ao clicar no botão de excluir.

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -32,10 +32,30 @@ class ProductItem extends StatelessWidget {
             ),
             IconButton(
               onPressed: () {
-                Provider.of<ProductList>(
-                  context,
-                  listen: false,
-                ).removeProduct(product);
+                showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                          title: Text('Exclusão de Produto'),
+                          content: Text('Deseja remover o produto?'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(ctx).pop(false);
+                              },
+                              child: Text('Não'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Provider.of<ProductList>(
+                                  context,
+                                  listen: false,
+                                ).removeProduct(product);
+                                Navigator.of(ctx).pop(true);
+                              },
+                              child: Text('Sim'),
+                            ),
+                          ],
+                        ));
               },
               icon: Icon(Icons.delete),
               color: Theme.of(context).colorScheme.error,

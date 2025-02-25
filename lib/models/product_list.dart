6617e9a850f8c03cbdf248630aa0e34085a9bb33
oJaciel <jacielhbarichellO@gmail.com>
2025-02-25 20:57:04 -0300

    Obtendo Dados do FIrebase
    Criado e implementado método para captar os produtos que estão cadastrados no Firebase.
    
    Agora apenas os produtos do banco são mostrados na aplicação.
    
    Implementada também tela de carregamento, para feedback enquanto usuário espera os dados serem captados.

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -2,13 +2,12 @@ import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 import 'package:http/http.dart' as http;
 
 class ProductList with ChangeNotifier {
-  List<Product> _items = dummyProducts;
-  final _baseUrl = 'https://shop-jaciel-default-rtdb.firebaseio.com';
+  List<Product> _items = [];
+  final _url = 'https://shop-jaciel-default-rtdb.firebaseio.com/products.json';
 
   List<Product> get items => [..._items]; // Os ... significam 'clonar' a lista
   List<Product> get favoriteItems =>
@@ -16,6 +15,27 @@ class ProductList with ChangeNotifier {
 
   int get itemsCount {
     return _items.length;
+  }
+
+  //Método para carregar os produtos do Firebase
+  Future<void> loadProducts() async {
+    _items.clear();
+    final response = await http.get(Uri.parse(_url));
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((productId, productData) {
+      _items.add(
+        Product(
+          id: productId,
+          name: productData['name'],
+          description: productData['description'],
+          price: productData['price'],
+          imageUrl: productData['imageUrl'],
+          isFavorite: productData['isFavorite'],
+        ),
+      );
+    });
+    notifyListeners();
   }
 
   Future<void> saveProduct(Map<String, Object> data) {
@@ -57,7 +77,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> addProduct(Product product) async {
     final response = await http.post(
-      Uri.parse('$_baseUrl/products.json'),
+      Uri.parse(_url),
       body: jsonEncode(
         {
           "name": product.name,

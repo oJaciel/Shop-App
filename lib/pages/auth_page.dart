15dbf3364127243726a4ec930bd81a958a278a40
oJaciel <jacielhbarichellO@gmail.com>
2025-04-23 20:48:07 -0300

    Tela de Autenticação - Início
    Iniciada a criação da tela de autenticação (login) da aplicação.
    
    Criado esboço base da tela e cadastradas rotas.
    
    Essa tela será mostrada sobre a tela inicial.

---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color.fromRGBO(215, 117, 255, 0.5),
+                Color.fromRGBO(215, 188, 117, 0.9),
+              ])
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            child: Column(
+              children: [
+                Center(child: Text('Essa é a tela de login'),)
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

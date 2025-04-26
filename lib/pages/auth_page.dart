8a50557f4a529f05fd98a3e6bf33ce63857fa652
oJaciel <jacielhbarichellO@gmail.com>
2025-04-25 21:43:26 -0300

    Finalização Tela Auth
    Finalizada a modelagem visual da tela de autenticação.
    
    Adicionado card de título.
    
    Criado componente que contém o formulário de autenticação.
    
    Modelado formulário para casos de login e registro funcionarem em um só componente.

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:shop/components/auth_form.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
@@ -10,17 +13,41 @@ class AuthPage extends StatelessWidget {
         children: [
           Container(
             decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [
-                Color.fromRGBO(215, 117, 255, 0.5),
-                Color.fromRGBO(215, 188, 117, 0.9),
-              ])
-            ),
+                gradient: LinearGradient(colors: [
+              Color.fromRGBO(215, 117, 255, 0.5),
+              Color.fromRGBO(215, 188, 117, 0.9),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
           ),
           Container(
             width: double.infinity,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Center(child: Text('Essa é a tela de login'),)
+                Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 70),
+                      transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.deepOrange.shade900,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 8,
+                          color: Colors.black26,
+                          offset: Offset(0, 2)
+                        )
+                      ]),
+                  child: Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                        fontSize: 45,
+                        fontFamily: 'Anton',
+                        color: Theme.of(context).colorScheme.onSecondary),
+                  ),
+                ),
+                AuthForm(),
               ],
             ),
           ),

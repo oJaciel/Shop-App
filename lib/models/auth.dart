aa5798be94a68c749f7bb9b306d10f42f4651439
oJaciel <jacielhbarichellO@gmail.com>
2025-04-27 20:06:24 -0300

    Login de Usuário
    Criado método para login do usuário.
    
    Modificado método de registoro, para reaproveitamento de código.

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -4,18 +4,34 @@ import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
-  static const _url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDoDWtdwKf-nYBYY8FsDKwztfZzE2gMkPk';
-  Future<void> signup(String email, String password) async {
-    final response = await http.post(
-      Uri.parse(_url),
-      body: jsonEncode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      }),
-    );
+  //URL do registro
+  //https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDoDWtdwKf-nYBYY8FsDKwztfZzE2gMkPk
+
+  //URL do login
+  //https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDoDWtdwKf-nYBYY8FsDKwztfZzE2gMkPk
+
+  Future<void> _authenticate(
+      String email, String password, String urlFragment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=AIzaSyDoDWtdwKf-nYBYY8FsDKwztfZzE2gMkPk';
+
+    final response = await http.post(Uri.parse(url),
+        body: jsonEncode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ));
 
     print(jsonDecode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    _authenticate(email, password, 'signInWithPassword');
   }
 }

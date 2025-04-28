e924429fe2ede9ba56752fd71ec92abffa75aaff
oJaciel <jacielhbarichellO@gmail.com>
2025-04-28 19:13:22 -0300

    Gerenciando Autenticação
    Captando dados recebidos na resposta de um login / registro bem sucedido.
    
    Utilizando os dados para verificar se o usuário está autenticado.
    
    Criada página que verifica se usuário está autenticado e retorna ou a tela de autenticação ou a tela inicial.

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -5,11 +5,28 @@ import 'package:http/http.dart' as http;
 import 'package:shop/exceptions/auth_exception.dart';
 
 class Auth with ChangeNotifier {
-  //URL do registro
-  //https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDoDWtdwKf-nYBYY8FsDKwztfZzE2gMkPk
+  String? _token;
+  String? _email;
+  String? _uid;
+  DateTime? _expiryDate;
 
-  //URL do login
-  //https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDoDWtdwKf-nYBYY8FsDKwztfZzE2gMkPk
+  bool get isAuth {
+    //Se a data de expiração do token é válida e existe token de autenticação, o usuário está autenticado.
+    final isValid = _expiryDate?.isAfter(DateTime.now()) ?? false;
+    return _token != null && isValid;
+  }
+
+  String? get token {
+    return isAuth ? _token : null;
+  }
+
+  String? get email {
+    return isAuth ? _email : null;
+  }
+
+  String? get uid {
+    return isAuth ? _uid : null;
+  }
 
   Future<void> _authenticate(
       String email, String password, String urlFragment) async {
@@ -27,10 +44,19 @@ class Auth with ChangeNotifier {
 
     final body = jsonDecode(response.body);
 
-    print(body);
-
     if (body['error'] != null) {
       throw AuthException(body['error']['message']);
+    } else {
+      _token = body['idToken'];
+      _email = body['e-mail'];
+      _uid = body['localId'];
+
+      _expiryDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(body['expiresIn']),
+        ),
+      );
+      notifyListeners();
     }
   }
 

c97dd86f7cf42f3a7c80392a8e2c2ed85f41cce5
oJaciel <jacielhbarichellO@gmail.com>
2025-06-22 18:06:25 -0300

    Adicionando Animações
    Adicionando mais animações na tela de autenticação.
    
    Usando AnimatedContainer, para facilitar o processo de implementação da animação.
    
    Pequena correção na tela de autenticação, para melhoria na responsividade.

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -18,37 +18,41 @@ class AuthPage extends StatelessWidget {
               Color.fromRGBO(215, 188, 117, 0.9),
             ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
           ),
-          Container(
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(bottom: 20),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 70),
-                      transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.deepOrange.shade900,
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 8,
-                          color: Colors.black26,
-                          offset: Offset(0, 2)
-                        )
-                      ]),
-                  child: Text(
-                    'Minha Loja',
-                    style: TextStyle(
-                        fontSize: 45,
-                        fontFamily: 'Anton',
-                        color: Theme.of(context).colorScheme.onSecondary),
-                  ),
+          Center(
+            child: SingleChildScrollView(
+              child: Container(
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: 20),
+                      padding:
+                          const EdgeInsets.symmetric(vertical: 10, horizontal: 70),
+                          transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.deepOrange.shade900,
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 8,
+                              color: Colors.black26,
+                              offset: Offset(0, 2)
+                            )
+                          ]),
+                      child: Text(
+                        'Minha Loja',
+                        style: TextStyle(
+                            fontSize: 45,
+                            fontFamily: 'Anton',
+                            color: Theme.of(context).colorScheme.onSecondary),
+                      ),
+                    ),
+                    AuthForm(),
+                  ],
                 ),
-                AuthForm(),
-              ],
+              ),
             ),
           ),
         ],

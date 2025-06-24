2aede10723a93aea34131f4e98c2b72350d48e89
oJaciel <jacielhbarichellO@gmail.com>
2025-06-23 21:57:15 -0300

    Animação na Tela de Pedidos
    Adicionada animação na tela de pedidos, para o botão de expandir.

--- lib/components/order_item.dart
+++ lib/components/order_item.dart
@@ -16,28 +16,33 @@ class _OrderItemState extends State<OrderItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
-            subtitle:
-                Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)),
-            trailing: IconButton(
-                onPressed: () {
-                  setState(() {
-                    _expanded = !_expanded;
-                  });
-                },
-                icon: Icon(Icons.expand_more)),
-          ),
-          if (_expanded == true)
-            Container(
+    final itemsHeight = (widget.order.products.length * 24.0) +
+        10; //A quantidade de produts * 25, + 10 (pra garanti que dê um espaço no final)
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _expanded ? itemsHeight + 80 : 80,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
+              subtitle: Text(
+                  DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)),
+              trailing: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _expanded = !_expanded;
+                    });
+                  },
+                  icon: Icon(Icons.expand_more)),
+            ),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              height: _expanded ? itemsHeight : 0,
               padding: const EdgeInsets.symmetric(
                 horizontal: 15,
                 vertical: 4,
               ),
-              height: (widget.order.products.length * 25.0) + 10, //A quantidade de produts * 25, + 10 (pra garanti que dê um espaço no final)
               child: ListView(
                 children: widget.order.products.map(
                   (product) {
@@ -64,7 +69,8 @@ class _OrderItemState extends State<OrderItem> {
                 ).toList(),
               ),
             )
-        ],
+          ],
+        ),
       ),
     );
   }

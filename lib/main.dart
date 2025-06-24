9d2c9072232f26e5da0adc2afb3cc7b1c1052cd9
oJaciel <jacielhbarichellO@gmail.com>
2025-06-23 22:06:03 -0300

    Transição ao Mudar Tela
    Adicionada transição ao mudar as telas da aplicação, no Android e IOS.

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/product_form_page.dart';
 import 'package:shop/pages/products_page.dart';
 import 'package:shop/utils/app_routes.dart';
+import 'package:shop/utils/custom_route.dart';
 
 void main() {
   runApp(const MyApp());
@@ -61,6 +62,12 @@ class MyApp extends StatelessWidget {
           ),
           fontFamily: 'Lato',
           useMaterial3: true,
+          pageTransitionsTheme: PageTransitionsTheme(
+            builders: {
+              TargetPlatform.android: CustomPageTransitionsBuilder(),
+              TargetPlatform.iOS: CustomPageTransitionsBuilder(),
+            }
+          )
         ),
         routes: {
           AppRoutes.AUTH_OR_HOME: (ctx) => AuthOrHomePage(),
